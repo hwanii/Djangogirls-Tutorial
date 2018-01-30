31f67f6677c7e5bc2ba277511504a6492cc4e2f1
Ryu Taehwan <yth3260@gmail.com>
2018-01-30 16:50:42 +0900

    post_detail로 이동하는 링크를 url태그를 이용해서 동적으로 생성

--- django/blog/urls.py
+++ django/blog/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, re_path
 from . import views
 import re
+
 urlpatterns = [
-    path('', views.post_list), # 함수 자체를 전달. 응답을 전달하면 다시 실행할 수 없음
-    # path('detail/', views.post_detail),
-
-
-    re_path(r'(?P<pk>\d+)/$', views.post_detail),
+    path('list', views.post_list, name='post-list'),  # 함수 자체를 전달. 응답을 전달하면 다시 실행할 수 없음
+    # re_path(r'(?P<pk>\d+)/$', views.post_detail),
+    path('post/<int:pk>/', views.post_detail, name='post-detail'),
 ]
