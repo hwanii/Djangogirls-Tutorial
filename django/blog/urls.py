013d6b9a70a9e6251e864a05069d0c9d3bb651f3
Ryu Taehwan <yth3260@gmail.com>
2018-01-30 15:59:00 +0900

    post_detail에 동적으로 pk값을 URL을 통해 받을 수 있도록 처리

--- django/blog/urls.py
+++ django/blog/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
-
+import re
 urlpatterns = [
     path('', views.post_list), # 함수 자체를 전달. 응답을 전달하면 다시 실행할 수 없음
-    path('detail/', views.post_detail)
+    # path('detail/', views.post_detail),
+
+
+    re_path(r'(?P<pk>\d+)/$', views.post_detail),
 ]
--- django/blog/views.py
+++ django/blog/views.py
@@ -27,5 +27,8 @@ def post_list(request):
     # 위 return 코드와 같음
     # return render(request, 'blog/post_list.html', context)
 
-def post_detail(request):
-    return render(request, 'blog/post_detail.html')
+def post_detail(request, pk):
+    context = {
+        'post': Post.objects.get(pk=pk),
+    }
+    return render(request, 'blog/post_detail.html', context)
