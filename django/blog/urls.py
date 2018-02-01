ff5196d70f42f7d8ad72703c87b5a404623b2ec8
Ryu Taehwan <yth3260@gmail.com>
2018-02-01 11:05:58 +0900

    base.html에 중복 HTML작성 및 extends로 템플릿 확장 사용

--- django/blog/urls.py
+++ django/blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 import re
 
 urlpatterns = [
-    path('list', views.post_list, name='post-list'),  # 함수 자체를 전달. 응답을 전달하면 다시 실행할 수 없음
+    path('list/', views.post_list, name='post-list'),  # 함수 자체를 전달. 응답을 전달하면 다시 실행할 수 없음
     # re_path(r'(?P<pk>\d+)/$', views.post_detail),
     path('post/<int:pk>/', views.post_detail, name='post-detail'),
 ]
